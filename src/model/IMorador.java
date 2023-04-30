331e910f3252a79f093fc95c99c18d62053b3b97
jonat <jonat@Jonathan>
2023-04-30 12:20:07 -0300

    Postagem

---
+++ src/model/IMorador.java
@@ -0,0 +1,18 @@
+package model;
+
+public interface IMorador {
+
+	
+	public String getnome();
+	public void setnome(String nome);
+
+	public int getcelular();
+	public void setcelular(int celular);
+	
+	public int getcpf();
+	public void setcpf(int cpf);
+	
+	public String getsituacao();
+	public void setsituacao(String sitacao);
+	
+}
---
+++ src/model/Morador.java
@@ -0,0 +1,73 @@
+package model;
+
+public class Morador implements IMorador {
+	
+	private String nome;
+    private int celular;
+    private int cpf;
+    private String situacao;
+
+    
+    public Morador (String nome,int celular,String situacao,int cpf){
+        this.nome = nome;
+        this.celular = celular;
+        this.situacao = situacao;
+        this.cpf = cpf;
+    }
+
+
+	@Override
+	public String getnome() {
+		// TODO Auto-generated method stub
+		return nome;
+	}
+
+
+	@Override
+	public void setnome(String nome) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public int getcelular() {
+		// TODO Auto-generated method stub
+		return celular;
+	}
+
+
+	@Override
+	public void setcelular(int celular) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public int getcpf() {
+		// TODO Auto-generated method stub
+		return cpf;
+	}
+
+
+	@Override
+	public void setcpf(int cpf) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public String getsituacao() {
+		// TODO Auto-generated method stub
+		return situacao;
+	}
+
+
+	@Override
+	public void setsituacao(String sitacao) {
+		// TODO Auto-generated method stub
+		
+	}
+}
